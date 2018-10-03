dc49330ced47a43b470aca42bbf933e308a7b808
filiphag <filiphag@stud.ntnu.no>
2018-10-03 18:43:24 +0200

    first 2 tests for "App"

--- Prosjekt3/components/__tests__/StyledText-test.js
+++ Prosjekt3/components/__tests__/StyledText-test.js
@@ -1,10 +1,10 @@
 import 'react-native';
 import React from 'react';
-import { MonoText } from '../StyledText';
+import App from '../../App';
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
+  const tree = renderer.create(<App />).toJSON();
 
   expect(tree).toMatchSnapshot();
 });
