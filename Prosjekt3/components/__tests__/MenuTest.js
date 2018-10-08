4c698412b5237dbb3397d6587221b81324acd828
filiphag <filiphag@stud.ntnu.no>
2018-10-08 14:41:57 +0200

    test fix
    #22
    Var en feil på navnet til menu => Menu

--- Prosjekt3/components/__tests__/MenuTest.js
+++ Prosjekt3/components/__tests__/MenuTest.js
@@ -1,7 +1,7 @@
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Menu from '../../components/menu';
+import Menu from '../../components/Menu';
 
 it('renders correctly', () => {
     const tree = renderer.create(<Menu>Snapshot test!</Menu>).toJSON();
