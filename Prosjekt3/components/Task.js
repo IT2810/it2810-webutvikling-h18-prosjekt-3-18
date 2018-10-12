5b077d19392495b4bca17cc84e1869db255aaeca
Truls Andreas Berglund <trulsabe@stud.ntnu.no>
2018-10-12 09:06:48 +0200

    #19 Getting storage to work so that one can store locally. Can delete and add to storage.

--- Prosjekt3/components/Task.js
+++ Prosjekt3/components/Task.js
@@ -3,13 +3,8 @@ import { View, Text, StyleSheet, FlatList, Button, TextInput } from 'react-nativ
 import CheckBox from 'react-native-check-box'
 
 class Task extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-
-    componentDidMount() {
-
+    constructor() {
+        super();
     }
 
     render() {
@@ -17,11 +12,7 @@ class Task extends Component {
             <View>
                 <CheckBox
                     isChecked={this.props.checked}
-                    onClick={(e) => {
-                        console.log("trykket");
-                        this.props.handleCheckbox(this.props.id)
-                    }
-                    }
+                    onClick={() => {this.props.handleCheckbox(this.props.id)}}
                 />
             </View>
         )
